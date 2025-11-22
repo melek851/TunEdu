0b5724f433adcf01a57c3aa5c08a3da7d1a0b7fd
Malek Bradai <malekbradai@gmail.com>
2025-11-22 09:54:11 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '..

--- src/app/admin/subjects/[subjectId]/lessons/new/page.tsx
+++ src/app/admin/subjects/[subjectId]/lessons/new/page.tsx
@@ -1,7 +1,7 @@
 
 import { getSubjectById } from '@/lib/firestore-data';
 import { notFound } from 'next/navigation';
-import { LessonForm } from '../_components/lesson-form';
+import { LessonForm } from '@/app/admin/subjects/_components/lesson-form';
 import { ArrowLeft } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
