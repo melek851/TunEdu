fb73a2c71258b9879ac30157d8057b85f7205693
Malek Bradai <malekbradai@gmail.com>
2025-12-13 17:15:09 +0000

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   images: {
     remotePatterns: [
       {
@@ -23,7 +20,7 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'picsum.photos',
         port: '',
-        pathname: '/**',
+pathname: '/**',
       },
     ],
   },
